6fcc0bb2342a83746b74375e24ace93a99d4b9fc
maherr404 <essalala01@gmail.com>
2025-01-16 18:07:07 +0200

    connect mysql by pdo

---
+++ pdo.php
@@ -0,0 +1,65 @@
+<?php 
+
+// connect mysql by pdo task
+
+class dp{
+
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "root";
+    private $dp = "fuck" ;
+
+
+    function connection(){
+
+        try {
+            $conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->dp, $this->username, $this->password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn ;
+            // echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+    }
+
+}
+// $obj = new dp;
+// echo $obj->connection();
+
+class user extends dp{
+    function selectdata(){
+        // return $this -> connection();
+        $sql = "select * from lala";
+        $stat = $this -> connection() -> query($sql);        //query()
+        $fetch = $stat -> fetchAll(PDO::FETCH_ASSOC);
+        return $fetch ;
+    }
+
+    function insertdata(){
+
+        $sql = "insert into lala (name , email , password) values (?,?,?)";
+        $statment = $this -> connection() -> prepare($sql);             // prepare()
+        $x = $statment -> execute(['abeer','abeer@gmail','abeer']);
+        if($x){
+            echo "inserted successfully";
+        }
+
+    }
+
+}
+// $obj1 = new user;
+// print_r($obj1->insertdata());
+
+$obj1 = new user;
+print_r($obj1->selectdata());
+
+
+
+
+
+
+
+
+?>
